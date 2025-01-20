653ab4ecd4213d71a9f26cc6e1180fdd99d6a1af
Alejandro Mira <alejomira96@gmail.com>
2025-01-19 19:06:22 -0500

    clase 8

---
+++ curso-intro-ts/src/03-demo3.ts
@@ -0,0 +1,7 @@
+//Esta es una funcion autoinvocada para limitar el alcance de las variables
+
+(() => {
+  let myProductName = 'product name';
+  let myProductPrice = 123;
+})()
+
---
+++ curso-intro-ts/src/03-typoing.ts
@@ -0,0 +1,12 @@
+//Este error se genera por que ya se declaro la variable myProductName y no se puede volver a declarar con let, se puede cambiar el valor pero no declarar nuevamente la variable
+let myProductName = 'My Product Name'
+let myProductPrice = 123
+
+myProductName = 'change'
+myProductName.toLowerCase()
+
+myProductPrice.toFixed()
+
+const myProductStock = 100
+
+
