986c0667fab85d08c4f0e3c4e566fdfd73327c48
Alejandro Mira <alejomira96@gmail.com>
2025-01-21 12:24:23 -0500

    clase 15

--- curso-intro-ts/src/10-alias.ts
+++ curso-intro-ts/src/10-alias.ts
@@ -1,3 +1,21 @@
 (() => {
-  let userID: string | number;
+  type ID = string | number;
+  let userID: ID;
+
+  function greeting(myText: ID) {
+    typeof myText === 'string'
+      ? console.log(`Hello ${myText}`)
+      : console.log(`Hello ${myText.toString()}`)
+  }
+  greeting('Hello World')
+  greeting(123456)
+
+  // literal types
+  type Sizes = 's' | 'm' | 'l';
+  let shirtSize: Sizes;
+  shirtSize = 's';
+  shirtSize = 'm';
+  shirtSize = 'l';
+  // shirtSize = 'xl'; // Error
+  // shirtSize = 'S'; // Error
 })()
