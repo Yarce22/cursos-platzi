bb48ded14b81c966ede78958e331ec6981fcd929
Alejandro Mira <alejomira96@gmail.com>
2025-01-21 11:44:27 -0500

    clase 13

---
+++ curso-intro-ts/src/08-any.ts
@@ -0,0 +1,9 @@
+(() => {
+  let myDynamicVar: any
+  myDynamicVar = 'Hello World'
+  console.log(myDynamicVar);
+  myDynamicVar = 10
+  console.log(myDynamicVar);
+  myDynamicVar = true
+  console.log(myDynamicVar);
+})()
