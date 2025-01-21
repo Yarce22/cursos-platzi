0b4b12dc82d8cb7f3c28f4c9163163bb7e5908f5
Alejandro Mira <alejomira96@gmail.com>
2025-01-21 10:49:53 -0500

    Clase 11

---
+++ curso-intro-ts/src/06-string.ts
@@ -0,0 +1,10 @@
+(() => {
+  let productTitle = 'My amazing product'
+  // productTitle = 123
+  // productTitle = null
+  // productTitle () => {}
+  productTitle = 'My new amazing product'
+  console.log('productTitle', productTitle);
+
+  const myString: string = 'Hello World'
+})()
