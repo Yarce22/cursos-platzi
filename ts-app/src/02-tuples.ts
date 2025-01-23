2d5e99ebd1404dc7f7f699084f35cf0b2058473e
Alejandro Mira <alejomira96@gmail.com>
2025-01-23 15:14:46 -0500

    clase 4

---
+++ ts-app/src/02-tuples.ts
@@ -0,0 +1,8 @@
+const prices: number[] = []
+
+const user: [string, number] = ['nico', 12]
+
+//A estas tuples podemos hacer destructuración
+const [username, age] = user
+console.log(username);
+console.log(age);
