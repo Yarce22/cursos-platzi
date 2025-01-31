5b9c69206f56bd62d6233de4178a89e2c76fbc24
Alejandro Mira <alejomira96@gmail.com>
2025-01-31 11:44:20 -0500

    clase 6

--- curso-react-ts/app/page.tsx
+++ curso-react-ts/app/page.tsx
@@ -1,15 +1,26 @@
+'use client'
 import { RandomFox } from "@/components/RandomFox";
+import { useState } from "react";
 const random = (): number => Math.floor(Math.random()*123)+1
 
 export default function Home() {
-  const srcImage = `https://randomfox.ca/images/${random()}.jpg`
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ])
+
+  // const srcImage = `https://randomfox.ca/images/${random()}.jpg`
 
   return (
     <div>
       <main>
-        <RandomFox
-          src={srcImage}
-        />
+        {images.map((image, index) => (
+          <div key={index} className="p-4">
+            <RandomFox src={image} />
+          </div>
+        ))}
       </main>
       <footer>
        
