5e2dc39534e7809ac0be7564784a489f684085bd
Alejandro Mira <alejomira96@gmail.com>
2025-01-31 14:02:47 -0500

    clase 10

--- curso-react-ts/components/RandomFox/index.tsx
+++ curso-react-ts/components/RandomFox/index.tsx
@@ -1,16 +1,39 @@
 // import type {FunctionComponent, FC } from 'react'
 import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react'
 
 type Props = {src: string}
 
 const RandomFox = ({ src }: Props): React.JSX.Element => {
+  const node = useRef<HTMLImageElement>(null)
+  const [image, setImage] = useState('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=')
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setImage(src)
+        }
+      })
+    })
+
+    if (node.current) {
+      observer.observe(node.current)
+    }
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [src])
 
   return (
     <Image
-      src={src}
+      ref={node}
+      src={image}
       alt="fox-image"
       width={400}
       height={400}
+      className='bg-gray-300'
     />
   )
 }
