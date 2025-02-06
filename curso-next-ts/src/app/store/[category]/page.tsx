f6513f60d715cff9206ed913f301cd2c43b219fc
Alejandro Mira <alejomira96@gmail.com>
2025-02-06 17:07:56 -0500

    Continuacion curso Next 14. Clase 4

---
+++ curso-next-ts/src/app/store/[category]/page.tsx
@@ -0,0 +1,7 @@
+const Category = (): React.JSX.Element => {
+  return (
+    <h1>Categoría dinámica</h1>
+  )
+}
+
+export default Category
---
+++ curso-next-ts/src/app/store/page.tsx
@@ -0,0 +1,7 @@
+const Store = (): React.JSX.Element => {
+  return (
+    <h1>Store</h1>
+  )
+}
+
+export default Store
