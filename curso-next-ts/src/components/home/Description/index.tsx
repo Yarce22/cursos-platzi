f37d9c938314344a72011a2d389736c61c98dfe1
Alejandro Mira <alejomira96@gmail.com>
2025-02-09 20:09:24 -0500

    clase 15

--- curso-next-ts/src/components/home/Description/index.tsx
+++ curso-next-ts/src/components/home/Description/index.tsx
@@ -4,13 +4,14 @@ import style from "./style.module.css"
 const Description = () => {
   return ( 
     <section className={style.Description}>
-      <Image
-        src="/images/description.jpeg"
-        alt="products marketplace"
-        width={500}
-        height={300}
-        priority={false}
-      />
+      <div className={style.Description__imageContainer}>
+        <Image
+          src="/images/description.jpeg"
+          alt="products marketplace"
+          priority={false}
+          fill
+        />
+      </div>
       <div>
         <h2>Description</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos molestiae omnis corporis. Iusto aliquam nemo fugit beatae? Eos doloremque dignissimos error explicabo, odit possimus reiciendis consequuntur consectetur accusamus magnam eaque.</p>
