3882fd92227ab391ffa2c6adc3c15440dfe066fb
Alejandro Mira <alejomira96@gmail.com>
2025-02-18 16:04:19 -0500

    Comienzo curso de react avanzado

--- curso-next-ts/src/components/shared/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>Future World</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- curso-next-ts/src/config/env.ts
+++
@@ -1,4 +0,0 @@
-export const env = {
-  SHOPIFY_HOSTNAME: process.env.SHOPIFY_HOSTNAME || "",
-  SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY || "", 
-}
