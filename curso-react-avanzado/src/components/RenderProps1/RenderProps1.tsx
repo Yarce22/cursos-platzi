5609c5f48f6e0214baa3d970b1f34a3b987c2c2e
Alejandro Mira <alejomira96@gmail.com>
2025-02-19 14:15:46 -0500

    Clase 2

--- curso-react-avanzado/src/components/RenderProps1/RenderProps1.tsx
+++ curso-react-avanzado/src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,20 @@
+type ChildParentComponentProps = {
+  render: (name: string) => JSX.Element;
+}
+
+export const ChildParentComponent: React.FC<ChildParentComponentProps> = ({ render }) => {
+  const name = 'Teffcode'
+  return (
+    <div>
+      {render(name)}
+    </div>
+  );
+};
+
 export const ParentComponent = () => {
   return (
     <>
+      <ChildParentComponent render={(name) => <p>Hello, {name}!</p>} />
     </>
   );
 };
