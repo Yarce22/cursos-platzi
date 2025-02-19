5beaf63e181355f1708b6b49cd9b50ddf91bd6a7
Alejandro Mira <alejomira96@gmail.com>
2025-02-19 16:01:13 -0500

    Clase 4.2

--- curso-react-avanzado/src/components/CompoundComponents/Tab.tsx
+++ curso-react-avanzado/src/components/CompoundComponents/Tab.tsx
@@ -1,6 +1,13 @@
-const Tab = () => {
+interface TabProps {
+  children: React.ReactNode;
+  label: string;
+};
+
+const Tab: React.FC<TabProps> = ({ label, children }) => {
   return (
     <>
+      <em>{label}</em>
+      <span>{children}</span>
     </>
   );
 };
