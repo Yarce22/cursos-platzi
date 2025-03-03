10b36d23429fe8e8d4855b24b31751e24a4375d5
Alejandro Mira <alejomira96@gmail.com>
2025-03-03 10:59:48 -0500

    clase 8

--- curso-react-avanzado/src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ curso-react-avanzado/src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,21 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const handleSubmit = () => {
+    if (inputRef.current) {
+      alert(`Nuevo producto en el carrito: ${inputRef.current.value}`)
+    }
+  }
+
   return (
     <div>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Nombre del producto"
+      />
+      <button onClick={handleSubmit}>Añadir al carrito</button>
     </div>
   );
 };
