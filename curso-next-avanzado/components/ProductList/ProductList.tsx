32739afd128c1e649e48df49ab8c3c925a319531
Alejandro Mira <alejomira96@gmail.com>
2025-03-12 13:32:39 -0500

    next/legacy/image

--- curso-next-avanzado/components/ProductList/ProductList.tsx
+++ curso-next-avanzado/components/ProductList/ProductList.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 import Link from 'next/link'
-import Image from "next/legacy/image"
+import Image from "next/image"
 
 type ProductListProps = {
   products: TProduct[]
@@ -13,7 +13,14 @@ const mapProductsToCards = (products: TProduct[]) =>
       <Card
         as="a"
         header={name}
-        image={{ children: <Image src={image} width={333} height={333} /> }}
+        image={{ children: <Image
+          src={image}
+          width={333}
+          height={333}
+          style={{
+            maxWidth: "100%",
+            height: "auto"
+          }} /> }}
         meta={{
           children: <Card.Meta style={{ color: 'dimgray' }}>{price}</Card.Meta>,
         }}
