1a55515b728170bf1d2a907e4b1605931ca0da6e
Alejandro Mira <alejomira96@gmail.com>
2025-03-26 15:58:24 -0500

    Clase 15

--- curso-next-14/src/component/home/Description/index.tsx
+++ curso-next-14/src/component/home/Description/index.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image';
+import { PLACEHOLDER_IMAGE } from '../../../../public/images/blurImages/blurImages';
 import styles from './Description.module.sass';
 
 export const Description = () => {
   return (
     <section className={styles.Description}>
-      <Image
-        src="/images/description.jpeg"
-        alt="products marketplace"
-        width={500}
-        height={300}
-      />
+      <figure className={styles.Description__imageContainer}>
+        <Image
+          src="/images/description.jpeg"
+          alt="products marketplace"
+          placeholder='blur'
+          blurDataURL={PLACEHOLDER_IMAGE}
+          fill
+        />
+      </figure>
       <div className={styles.Description__text}>
         <h2>Bring the future today</h2>
         <p>{"Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us."}</p>
