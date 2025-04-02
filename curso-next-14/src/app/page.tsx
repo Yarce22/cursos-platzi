915b0067585a298de4adaee1426d6173ccef17a1
Alejandro Mira <alejomira96@gmail.com>
2025-04-01 19:24:57 -0500

    Clase 40

--- curso-next-14/src/app/page.tsx
+++ curso-next-14/src/app/page.tsx
@@ -2,9 +2,17 @@ import { Description } from "app/component/home/Description";
 import { Hero } from "app/component/home/Hero";
 import { MainProducts } from "app/component/home/MainProducts";
 import { Loader } from "app/component/shared/Loader";
+import { Metadata } from "next";
 import { Suspense } from "react";
 
+export const metadata: Metadata = {
+  title: "Future World",
+  description: "Welcome to the future e-commerce",
+  keywords: "e-commerce, shopping, online store, ecommerce, store, online shopping, online shop, e-commerce store, ecommerce store, shopping store",
+}
+
 export default function Home() {
+
   return (
     <main>
       <Hero />
--- curso-next-14/src/app/product/[handle]/page.tsx
+++ curso-next-14/src/app/product/[handle]/page.tsx
@@ -1,6 +1,7 @@
 import { ProductView } from "app/component/product/ProductView/ProductView";
 import { getProducts } from "app/services/shopify/products";
 import { redirect } from "next/navigation";
+import { title } from "process";
 
 
 interface ProductPageProps {
@@ -9,9 +10,32 @@ interface ProductPageProps {
   }
 }
 
+export async function generateMetadata({ searchParams }: ProductPageProps) {
+  const id = searchParams.id
+  const products = await getProducts(id)
+  const product = products[0]
+
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      title: product.title,
+      description: product.description,
+      images: [
+        {
+          url: product.image,
+          width: 1200,
+          height: 1200,
+          alt: product.title,
+        },
+      ],
+    },
+  }
+}
+
 export default async function ProductPage(props: ProductPageProps) {
   const id = props.searchParams.id
-  
   const products = await getProducts(id)
   const product = products[0]
 
