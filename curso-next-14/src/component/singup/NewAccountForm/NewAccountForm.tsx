01d7df244740ab298c7eecd9f3f4a5fa68a937e1
Alejandro Mira <alejomira96@gmail.com>
2025-04-02 15:10:47 -0500

    Clase 42

--- curso-next-14/src/component/singup/NewAccountForm/NewAccountForm.tsx
+++ curso-next-14/src/component/singup/NewAccountForm/NewAccountForm.tsx
@@ -8,18 +8,18 @@ export const NewAccountForm = () => {
   const [errors, setErrors] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const formData = new FormData(e.currentTarget)
-    handleCreateUser(formData)
+    await handleCreateUser(formData)
   }
 
   return (
     <div className={styles.NewAccountForm}  >
       <h1 className={styles.NewAccountForm__title}>New Account</h1>
       <form className={styles.NewAccountForm__form} onSubmit={handleSubmit}>
-        <input type="text" name="first_name" placeholder="Name" disabled={loading} />
-        <input type="text" name="last_name" placeholder="Lastname" disabled={loading} />
+        <input type="text" name="firstName" placeholder="Name" disabled={loading} />
+        <input type="text" name="lastName" placeholder="Last name" disabled={loading} />
         <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" disabled={loading} />
         <input type="text" name="phone" placeholder="phone number" pattern="^[0-9]*$" disabled={loading} />
         <input type="password" name="password" placeholder="password" disabled={loading} />
